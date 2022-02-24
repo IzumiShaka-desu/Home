dd2672005ce3c8e2df634a85c26b0c66ca3a2b31
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-24 22:18:19 +0700

    update itemlist

--- Sources/Home/presentation/components/ItemList.swift
+++ Sources/Home/presentation/components/ItemList.swift
@@ -1,20 +1,96 @@
 //
-//  SwiftUIView.swift
-//  
+//  ItemList.swift
+//  gemmu
 //
-//  Created by Akashaka on 24/02/22.
+//  Created by Akashaka on 09/02/22.
 //
 
 import SwiftUI
+import NetworkImage
+import Common
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct ItemList: View {
+  var title: String
+  var releaseDate: String
+  var platforms: [String]
+  var genres: [String]
+  var imageUrl: String
+  var id: Int
+  
+  var body: some View {
+    ZStack(alignment: .leading) {
+      Color.flatDarkCardBackground
+      HStack {
+        ZStack {
+          if #available(macOS 12.0, *) {
+            NetworkImage(url: URL(string: imageUrl)) { image in
+              image.resizable()
+            } placeholder: {
+              ProgressView()
+            } fallback: {
+              Image(systemName: "photo")                     }
+            .frame(width: 75, height: 75)
+            .clipped()
+            .background().cornerRadius(10)
+          } else {
+            NetworkImage(url: URL(string: imageUrl)) { image in
+              image.resizable()
+            } placeholder: {
+              ProgressView()
+            } fallback: {
+              Image(systemName: "photo")                     }
+            .frame(width: 75, height: 75)
+            .clipped()
+          }
+        }
+        .frame(width: 70, height: 70, alignment: .center)
+        
+        VStack(alignment: .leading) {
+          Text(title).foregroundColor(.white)
+            .font(.headline)
+            .fontWeight(.bold)
+            .lineLimit(2)
+            .padding(.bottom, 5)
+          Text(releaseDate)
+            .padding(.bottom, 5).foregroundColor(.white)
+          
+          HStack(alignment: .center) {
+            ForEach(platforms, id: \.self) { platform in
+              TagsCard(
+                text: platform,
+                bgColor: .green)
+            }
+          }
+          .padding(.bottom, 5)
+          
+          HStack {
+            ForEach(genres, id: \.self) { genre in
+              TagsCard(text: genre, bgColor: .pink)
+            }
+          }
+        }
+        .padding(.horizontal, 5)
+        
+      }
+      .padding(15)
+    }        .clipShape(RoundedRectangle(cornerRadius: 15))
+  }
+  
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
+struct ItemList_Previews: PreviewProvider {
+  static var previews: some View {
+    
+    let platforms: [String] = ["PS5", "PC"]
+    
+    ItemList(
+      title: "title",
+      releaseDate: "address",
+      platforms: platforms,
+      genres: ["action", "rpg"],
+      imageUrl: Constants.profileImageUrl,
+      id: 3498
+    )
+  }
 }
+
